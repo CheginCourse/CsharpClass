243d8c4e7b2fb1a094f820058d1f2ec0f3a2d9db
ubteenCheg <fire.flyers13@gmail.com>
2022-06-08 02:14:11 +0430

    session 8-overloading

---
+++ session8-overloading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace session8over // Note: actual namespace depends on the project name.
+{
+    internal class Progam
+    {
+//  oop Design 
+ static void Main(string[] args)
+        {
+
+  Add(3,6);
+  Add(3.5 , 9.2);
+  Add("asghar " , "akbari");
+
+
+        }
+
+
+        public static void Add(int x , int y){
+          int result = x + y ;
+         System.Console.WriteLine(result); 
+
+        }
+          public static void Add(Double x , Double y){
+          
+             Double result = x + y ;
+            System.Console.WriteLine(result); 
+        }
+          public static void Add(string firstname , string lastname){
+               string  result = $"{firstname}{lastname}" ;
+         System.Console.WriteLine(result); 
+          
+        }
+    }
+}
