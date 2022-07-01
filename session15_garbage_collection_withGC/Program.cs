b6d76d179399d478f4966a46d62e53e7c99bef04
ubteenCheg <fire.flyers13@gmail.com>
2022-07-01 22:17:39 +0430

    session 15 garbage with GC

---
+++ session15_garbage_collection_withGC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+
+
+  class GarbageTest
+  {
+    public GarbageTest()
+    {
+      
+    }
+    //Destructors
+    // Finalizer
+~GarbageTest()
+{
+
+System.Console.WriteLine("Destroy by Finalizer");
+}
+
+  }
+    internal class Program
+    {
+      public static CheckGarbageCollection(){
+        GarbageTest objGarbageTest = new GarbageTest();
+      }
+        static void Main(string[] args)
+        {
+           CheckGarbageCollection();
+           GC.Collect();
+           GC.WaitForPendingFinalizers();
+
+        }
+    }
+}
