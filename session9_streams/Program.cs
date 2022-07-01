64055350d4ba295f634b1b7df82ed89196e8050d
ubteenCheg <fire.flyers13@gmail.com>
2022-07-01 17:51:56 +0430

    session9 streams

---
+++ session9_streams/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;  
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          FileStream fs = new FileStream(@"/Users/abtin_cheg/Desktop/CsharpMainClass/CsharpClass/session9_streams/test.txt",FileMode.OpenOrCreate,FileAccess.ReadWrite);
+          fs.Seek(0,SeekOrigin.End);
+          ///----------------- Stream Writer -----------------///
+          StreamWriter objStreamWriter = new StreamWriter(fs);
+          objStreamWriter.WriteLine("hello");
+          objStreamWriter.AutoFlush = true;
+          ///-----------------Stream Reader ------------------///
+          using(StreamReader reader = new StreamReader(@"/Users/abtin_cheg/Desktop/CsharpMainClass/CsharpClass/session9_streams/test.txt")) {
+    string line;
+    while((line = reader.ReadLine()) != null) {
+      System.Console.WriteLine("Enter");
+  string result=  reader.ReadToEnd();
+         System.Console.WriteLine(result);
+  
+    }
+}
+        
+       
+
+           
+        }
+    }
+}
