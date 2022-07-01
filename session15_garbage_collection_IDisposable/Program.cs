427530701e0928a1c9385ca4235f31e228570b24
ubteenCheg <fire.flyers13@gmail.com>
2022-07-01 22:24:36 +0430

    session 15 garbage with Idispose

---
+++ session15_garbage_collection_IDisposable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+
+
+  class GarbageTest : IDisposable
+  {
+    public GarbageTest() 
+    {
+      
+    }
+    public void Dispose(){
+      System.Console.WriteLine("Disposed");
+
+    }
+
+  }
+    internal class Program
+    {
+      public static void CheckGarbageCollection(){
+        using ( GarbageTest objGarbageTest = new GarbageTest())
+        {
+          
+        }
+       
+      }
+        static void Main(string[] args)
+        {
+           CheckGarbageCollection();
+            GarbageTest objGarbageTest = new GarbageTest();
+            objGarbageTest.Dispose();
+
+        }
+    }
+}
