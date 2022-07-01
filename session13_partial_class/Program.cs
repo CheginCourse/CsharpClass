61a40cedef0c4845694dbb7fe49840766454cfbd
ubteenCheg <fire.flyers13@gmail.com>
2022-07-01 21:29:44 +0430

    session 13 partial class

---
+++ session13_partial_class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+     partial class Merged
+      {
+        public int Age { get; set; }
+        public String Name { get; set; }
+        
+      }
+       partial class Merged
+      {
+        
+        public void SetName(int age , string name){
+
+          this.Age = age;
+          this.Name = name;
+          System.Console.WriteLine($"{Age} {Name}");
+
+        }
+      }
+
+        static void Main(string[] args)
+        {
+
+            Merged objMerged = new Merged();
+     
+            objMerged.SetName(23 , "akbar");
+
+        }
+    }
+}
