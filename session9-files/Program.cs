c6a1368d5822d11d88d3f88bbbb14c69d3498255
ubteenCheg <fire.flyers13@gmail.com>
2022-07-01 18:02:22 +0430

    session9 files

---
+++ session9-files/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make sure to change this folder to your own folder  
+                string folder = @"/Users/abtin_cheg/Desktop/CsharpMainClass/CsharpClass/session9-files/";  
+                // Filename  
+                string fileName = "test.txt";  
+                // Fullpath. You can direct hardcode it if you like.  
+                string fullPath = folder + fileName;  
+                // An array of strings  
+                string[] authors = {"Mahesh Chand", "Allen O'Neill", "David McCarter",  
+                "Raj Kumar", "Dhananjay Kumar"};  
+                // Write array of strings to a file using WriteAllLines.  
+                // If the file does not exists, it will create a new file.  
+                // This method automatically opens the file, writes to it, and closes file  
+                File.WriteAllLines(fullPath, authors);  
+                // Read a file  
+                string readText = File.ReadAllText(fullPath);  
+                Console.WriteLine(readText);  
+        }
+    }
+}
