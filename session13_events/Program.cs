23b608f89bccf79ac16e0b7f5fa73bbc14eef3f8
ubteenCheg <fire.flyers13@gmail.com>
2022-07-01 21:23:06 +0430

    session 13 events

---
+++ session13_events/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+  class EventClass
+  {
+     public event EventHandler onSpaceTrigger;
+     public void TestingSpaceTrigger ( Object sender , EventArgs e ){
+      System.Console.WriteLine("U key with event");
+     }
+     public void Test(){
+      var name = System.Console.ReadKey();
+      System.Console.WriteLine();
+      if (name.Key == ConsoleKey.U)
+      {
+   
+        onSpaceTrigger?.Invoke(this,EventArgs.Empty);
+      }
+
+     }
+
+  }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EventClass objEventClass = new EventClass();
+            objEventClass.onSpaceTrigger +=objEventClass.TestingSpaceTrigger;
+            objEventClass.Test();
+        }
+    }
+}
