732eeb60ebf3377dc1c9b382acd3b9c44b4ff76c
ubteenCheg <fire.flyers13@gmail.com>
2022-07-02 02:38:06 +0430

    session 15 nullable type

---
+++ session15_nullable_type/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime objDateTime= new DateTime();
+            // Nullable<DateTime> objNullableDatetime= new   Nullable<DateTime>();
+            // you can use DateTime? instead of Nullable<DateTime>
+            // Nullable<DateTime> ----> DateTime?
+            // this always true 
+             DateTime?  objNullableDatetime = objDateTime;
+             // but this one is not (Cannot implicitly convert type 'System.DateTime?' to 'System.DateTime')
+            //  DateTime objNewDatetime= objNullableDatetime;
+
+            System.Console.WriteLine(objNullableDatetime.GetValueOrDefault());
+            System.Console.WriteLine(objNullableDatetime.Value);
+
+        }
+    }
+}
