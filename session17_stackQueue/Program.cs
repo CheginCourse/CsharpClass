1945a4e810afbfd3528e4c7a8629f005cf1d29fd
ubteenCheg <fire.flyers13@gmail.com>
2022-07-06 10:36:30 +0430

    stack and queue in c#

---
+++ session17_stackQueue/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+        //   // Stack
+        //         Stack<string> stack1 = new Stack<string>();
+        //         stack1.Push("asdas");
+        //         stack1.Push("ddd");
+        //         stack1.Push("aaa");
+        //         stack1.Push("gggg");
+                
+        //        System.Console.WriteLine(stack1.Peek()); 
+        //         foreach (var item in stack1)
+        //         {
+        //             System.Console.WriteLine(item);
+        //         }
+
+        // Queue
+
+        Queue<string> queue1 = new Queue<string>();
+        queue1.Enqueue("1");
+         queue1.Enqueue("2");
+          queue1.Enqueue("3");
+           queue1.Enqueue("4");
+        foreach (var item in queue1)
+        {
+            System.Console.WriteLine(item);
+        }
+                
+                
+
+        }
+    }
+}
